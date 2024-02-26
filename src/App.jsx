2fc6a0cd003761ab97237332a8a005483b9878d7
Adnane Kh <adnangamas@gmail.com>
2024-02-26 01:42:06 +0100

    added products & finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import Header from './components/Header.jsx';
 import TabButton from './components/TabButton.jsx';
 import {CORE_CONCEPTS} from './data.js';
-import ClickButton from './components/TabButton.jsx';
+import {PRODUCTS} from './data.js';
 function CoreConcept({image,title,description}){
   return(
     <li>
@@ -12,6 +13,12 @@ function CoreConcept({image,title,description}){
   )
 }
 function App() {
+  const [selectedTopic, setSelectedTopic]=useState();
+
+  function handleSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header/>
@@ -26,10 +33,23 @@ function App() {
         <section id='products'>
           <h2>Products</h2>
           <menu>
-            <TabButton>Monitor</TabButton>
-            <TabButton>Pc</TabButton>
-            <TabButton>Accessories</TabButton>
+            <TabButton 
+            isSelected={selectedTopic==='monitor'} 
+            onSelect={() => handleSelect('monitor')}>Monitor</TabButton>
+            <TabButton 
+            isSelected={selectedTopic==='pc'}  
+            onSelect={() => handleSelect('pc')}>Pc</TabButton>
+            <TabButton 
+            isSelected={selectedTopic==='accessories'} 
+            onSelect={() => handleSelect('accessories')}>Accessories</TabButton>
           </menu>
+          {!selectedTopic && <p>Please select a button</p>}
+          {selectedTopic && (
+            <div id='tab-content'>
+            <img src = {PRODUCTS[selectedTopic].img} />
+            <h3>{PRODUCTS[selectedTopic].title}</h3>
+            <p>{PRODUCTS[selectedTopic].description}</p>
+          </div>) }
         </section>
       </main>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,6 @@
+import gamingMonitor from './assets/gamingMoitor.jpg';
+import gamingPc from './assets/gaminngPc.jpg';
+import gamingAccessories from './assets/gamingAccessories.jpg';
 import monitorImg from './assets/monitor.png';
 import pcCaseImg from './assets/pccase.jpg';
 import pcAccessoriesImg from './assets/pcaccessories.jpg';
@@ -22,3 +25,23 @@ export const CORE_CONCEPTS = [
       'Enhance your setup with premium PC accessories: mechanical keyboard with customizable RGB lighting, ergonomic gaming mouse with precision sensor, and noise-canceling headset for immersive audio. Elevate your gaming experience and productivity with these sleek and durable peripherals designed for peak performance.',
   },
 ];
+export const PRODUCTS = {
+  monitor: {
+    img: gamingMonitor,
+    title: 'Monitor (137$)',
+    description:
+      "EXCEPTIONAL PICTURE QUALITY: The 2560x1440 monitor with the DCI-P3 90% wide color gamut brings what you’re watching to life in over 16.7M colors. Besides, Gaming monitor with high color accuracy, no fading phenomenon, the colors of all angles are more realistic, detailed and consistent. Thus you can get the same image quality from any angle.",
+  },
+  pc: {
+    img: gamingPc,
+    title: 'Pc (1240$)',
+    description:
+    "INTEL Core i7 12700F 2.1GHz (4.9GHz Max Boost) CPU Processor | 1TB NVME – Up to 30x Faster Than Traditional HDD | 240mm AIO NVIDIA Geforce RTX 4060 Ti 8GB GDDR6X Graphics Card (Brand may vary) | 16GB DDR4 RAM 3200 Gaming Memory with Heat Spreader | Windows 11 Home 64-bit",
+  },
+  accessories: {
+    img: gamingAccessories,
+    title: 'Accessories (50$)',
+    description:
+      'Gaming Mouse and Keyboard and around the Head Gaming Headset and Mouse Mat Complete Starter pack all in one great Gift box (This Bundle includes FOUR items)',
+  },
+};
